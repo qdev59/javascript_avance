7826bca1f46a1dda54a0a1d1be5b24d226ff9713
qdev59 <qdevriendt@gmail.com>
2020-09-14 13:41:25 +0200

    Exo 3

--- src/exo03.js
+++ src/exo03.js
@@ -2,5 +2,9 @@
 // { a: "b" } => { b: "a" }
 
 export function invertKeysAndValues(obj) {
-  return obj;
+  var inverted = {};
+  for (var key in obj) {
+    inverted[obj[key]] = key;
+  }
+  return inverted;
 }
