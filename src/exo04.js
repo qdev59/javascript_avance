7ae068a8f4e2c96b2910ce7cbf138c41e271c21e
qdev59 <qdevriendt@gmail.com>
2020-09-14 14:23:07 +0200

    Exo 4

--- src/exo04.js
+++ src/exo04.js
@@ -3,10 +3,30 @@ export function addWatcher(obj, prop, getter, setter) {
   // - getter lorsque la propriété prop est accédée
   // - setter lorsque la propriété prop est modifiée
   // il doit toujours être possible d'écrire et de lire une valeur pour la propriété prop
+  var propValue = obj[prop];
+  Object.defineProperty(obj, prop, {
+    // Raccourci pour get: function()
+    get() {
+      getter(propValue);
+      return propValue;
+    },
+    // Raccourci pour set: function()
+    set(value) {
+      setter(value);
+      propValue = value;
+    }
+  });
 }
 
 export function setPrivatesAndConstants(obj) {
   // changer le descripteur de chaque propriété de l'objet:
   // énumérable si la clé ne commence pas par par un _
   // mutable et configurable si la clé ne commence pas par une majuscule (regex: /[A-Z]/)
+  for (var key in obj) {
+    Object.defineProperty(obj, key, {
+      enumerable: !key.startsWith("_"),
+      writable: !/[A-Z]/.test(key[0]),
+      configurable: !/[A-Z]/.test(key[0])
+    });
+  }
 }
