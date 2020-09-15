b6f0d98df04e7e471ad1e69f18906b6484637d00
qdev59 <qdevriendt@gmail.com>
2020-09-15 14:45:45 +0200

    Exo 13

--- src/exo13.js
+++ src/exo13.js
@@ -2,6 +2,13 @@
 // fn(function callback(error, result){ ... }, arg1, arg2);
 export function promisify(fn) {
   // TODO: retourner une fonction appelant fn mais
+  return (...args) =>
+    new Promise((resolve, reject) => {
+      const callback = (error, result) => {
+        return error ? reject(error) : resolve(result);
+      };
+      fn(callback, ...args);
+    });
   // retournant une Promise au lieu de passer un callback
 }
 
