b9a17b2504a623b8434c8784d30ca264ef8a249b
qdev59 <qdevriendt@gmail.com>
2020-09-16 11:52:05 +0200

    Exo 19

--- src/exo19.js
+++ src/exo19.js
@@ -2,7 +2,22 @@ export const addAliasForProperties = (object, alias) => {
   // TODO: retourner un Proxy pour l'objet permettant
   // de déclarer des alias pour accéder en lecture ou écriture
   // à une propriété de l'objet
-  return object;
+  return new Proxy(object, {
+    get(object, prop) {
+      const realProp = alias.hasOwnProperty(prop) ? alias[prop] : prop;
+      return Reflect.get(object, realProp);
+    },
+    set(object, prop, value) {
+      const realProp = alias.hasOwnProperty(prop) ? alias[prop] : prop;
+      return Reflect.set(object, realProp, value);
+    },
+    has(obj, prop) {
+      return (
+        Reflect.has(obj, prop) ||
+        (alias.hasOwnProperty(prop) && Reflect.has(obj, alias[prop]))
+      );
+    }
+  });
 };
 
 // exemple d'utilisation:
@@ -13,15 +28,24 @@ const user = addAliasForProperties(
 
 // `${user.firstName} ${user.lastName}` === "Rick Sanchez"
 
-export const countFunctionCalls = fn => {
+export const countFunctionCalls = (fn) => {
   // TODO: retourner un Proxy pour la fonction permettant
   // de compter le nombre d'appels faits pour cette fonction,
   // à récupérer via fn.count
-  return fn;
+  let count = 0;
+  return new Proxy(fn, {
+    get(o, key) {
+      return key === "count" ? count : Reflect.get(o, key);
+    },
+    apply(f, context, args) {
+      count++;
+      return Reflect.apply(f, context, args);
+    }
+  });
 };
 
 // exemple d'utilisation:
-const fn = countFunctionCalls(n => n * 2);
+const fn = countFunctionCalls((n) => n * 2);
 fn(1);
 fn(2);
 fn(999);
