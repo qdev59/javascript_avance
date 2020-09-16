6c20e16bc20d7900bc5fb9f49892e9536de1637e
qdev59 <qdevriendt@gmail.com>
2020-09-16 15:29:48 +0200

    Exo 21

--- src/exo21.js
+++ src/exo21.js
@@ -2,24 +2,29 @@ import data from "../__tests__/fakedata.json";
 
 // TODO: implémenter une fonction qui retourne la composée de plusieurs fonctions
 // compose(f,g,h)(...args) <=> h(g(f(...args)))
-export function compose() {
-
+export function compose(f1, ...functions) {
+  return function (...args) {
+    //return f1(...args);
+    return functions.reduce((resultatPrecedent, currentFn) => {
+      return currentFn(resultatPrecedent);
+    }, f1(...args));
+  };
 }
 
 // les fonctions composées doivent être pures et non mutables
-export const where = (field, condition, array) => array.filter(({ [field]: value }) => condition(value))
-export const orderBy = (field, array) => [...array].sort((a, b) => a[field] < b[field] ? -1 : 1);
-export const take = (number, array) => array.slice(0, number)
-export const map = (transform, array) => array.map(transform)
+export const where = (field, condition, array) =>
+  array.filter(({ [field]: value }) => condition(value));
+export const orderBy = (field, array) =>
+  [...array].sort((a, b) => (a[field] < b[field] ? -1 : 1));
+export const take = (number, array) => array.slice(0, number);
+export const map = (transform, array) => array.map(transform);
 
 // exemple d'utilisation
 const query = compose(
-    data => where("age", n => n >= 18, data),
-    results => orderBy("lastName", results),
-    results => take(5, results),
-    results => map(user => `${user.firstName} ${user.lastName}`, results)
-)
-
-console.log(
-    query(data).join("\n")
+  (data) => where("age", (n) => n >= 18, data),
+  (results) => orderBy("lastName", results),
+  (results) => take(5, results),
+  (results) => map((user) => `${user.firstName} ${user.lastName}`, results)
 );
+
+console.log(query(data).join("\n"));
